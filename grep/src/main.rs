19530037cd3b3d86cd73526bf74940990c49aa43
Mustaque Ahmed <amustaque97@gmail.com>
2023-12-24 01:47:16 +0530

    build grep basic setup step1 & step2

---
+++ grep/src/main.rs
@@ -0,0 +1,41 @@
+use std::{
+    fs,
+    io::{Read, Write},
+};
+
+use clap::Parser;
+use regex::Regex;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// keyword to look for
+    pattern: String,
+
+    file: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let pattern: String = args.pattern;
+    let file: String = args.file.unwrap_or("-".to_string());
+    let mut content = String::new();
+    match file.as_str() {
+        "-" => {
+            let mut stdin = std::io::stdin();
+            let _ = stdin.lock();
+            let mut buf = Vec::new();
+            stdin.read_to_end(&mut buf).unwrap();
+            content = String::from_utf8(buf).unwrap();
+        }
+        _ => content = fs::read_to_string(&file).unwrap().parse().unwrap(),
+    }
+
+    if pattern.is_empty() {
+        print!("{}", content);
+    } else {
+        let re = Regex::new(format!("\\b{}\\w*", pattern).as_str()).unwrap();
+        let matches: Vec<_> = re.find_iter(&content).map(|m| m.as_str()).collect();
+        println!("{:?}", matches);
+    }
+}
